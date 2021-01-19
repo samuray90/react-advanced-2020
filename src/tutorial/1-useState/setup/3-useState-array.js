9fd36822a61f4d2923e5d53c71358cb635fa6640
samuray90 <ionutsdaniel53@gmail.com>
2021-01-19 16:34:58 +0200

    onClick

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -13,6 +13,9 @@ const UseStateArray = () => {
           </div>
         );
       })}
+      <button className="btn" onClick={() => setPeople([])}>
+        clear it now
+      </button>
     </>
   );
 };

1e40f6fe65fac4cc07d66a358d72f7e7cc9ffbde
samuray90 <ionutsdaniel53@gmail.com>
2021-01-19 16:08:16 +0200

    watch ouut

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useState } from "react";
+import { listacurului } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(listacurului);
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default UseStateArray;
